3977542c8e4ba6f6a6687be06da11728e45e8958
Morgan Gallant <morgan@morgangallant.com>
2020-06-03 10:51:50 -0400

    ipn: basic implementation
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/operandai/ipn
+module github.com/gallantlabs/ipn
 
 go 1.14
+
+require tailscale.com v0.98.0
---
+++ ipn.go
@@ -0,0 +1,53 @@
+package ipn
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"runtime"
+
+	"tailscale.com/net/interfaces"
+)
+
+type Peer struct {
+	Host string `json:"HostName"`
+	Addr net.IP `json:"TailAddr"`
+	OS   string `json:"OS"`
+}
+
+func Me() (Peer, error) {
+	host, err := os.Hostname()
+	if err != nil {
+		return Peer{}, fmt.Errorf("ipn: failed to get machine hostname: %w", err)
+	}
+	addr, _, err := interfaces.Tailscale()
+	if err != nil {
+		return Peer{}, fmt.Errorf("ipn: failed to get tailscale interface: %w", err)
+	}
+	ops := runtime.GOOS
+	return Peer{
+		Host: host,
+		Addr: addr,
+		OS:   ops,
+	}, nil
+}
+
+func QueryForPeers() ([]Peer, error) {
+	out, err := exec.Command("tailscale", "status", "--json").Output()
+	if err != nil {
+		return nil, fmt.Errorf("ipn: failed to exec tailscale cmd: %w", err)
+	}
+	var buf struct {
+		Peers map[string]Peer `json:"Peer"`
+	}
+	if err = json.Unmarshal(out, &buf); err != nil {
+		return nil, fmt.Errorf("ipn: failed to unmarshal tailscale status: %w", err)
+	}
+	peers := make([]Peer, 0, len(buf.Peers))
+	for _, p := range buf.Peers {
+		peers = append(peers, p)
+	}
+	return peers, nil
+}
